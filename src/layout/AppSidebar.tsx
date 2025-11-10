4375f633b544ca94d8b9d0279d0f770e6f42e1f7
Your <your_email@example.com>
2025-11-10 13:35:47 +0800

    refactor: 删除 SidebarWidget 推广组件

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -16,7 +16,6 @@ import {
   UserCircleIcon,
 } from "../icons";
 import { useSidebar } from "../context/SidebarContext";
-import SidebarWidget from "./SidebarWidget";
 
 type NavItem = {
   name: string;
@@ -375,7 +374,6 @@ const AppSidebar: React.FC = () => {
             </div>
           </div>
         </nav>
-        {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null}
       </div>
     </aside>
   );
